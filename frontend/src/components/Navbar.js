27ee2c153134d241d2b2c34260d2024f221fb753
ameyrj445 <jainameyraj@gmail.com>
2026-04-21 18:51:35 +0530

    version v3

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
@@ -15,6 +21,25 @@ function Navbar() {
           JOBS
         </Link>
         <Link to="/internships" className="nav-link">
+          <span className="icon">
+            <img src="/internship.png" alt="Internships" className="nav-icon-image" />
+          </span>
+          INTERNSHIPS
+        </Link>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <div className={`mobile-menu ${isMenuOpen ? 'active' : ''}`}>
+        <Link to="/jobs" className="nav-link mobile-link" onClick={toggleMenu}>
+          <span className="icon">
+            <img src="/briefcase.png" alt="Jobs" className="nav-icon-image" />
+          </span>
+          JOBS
+        </Link>
+        <Link to="/internships" className="nav-link mobile-link" onClick={toggleMenu}>
           <span className="icon">
             <img src="/internship.png" alt="Internships" className="nav-icon-image" />
           </span>
